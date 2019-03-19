23a86d4e4b9e91c0351ec2d3932f44769735a904
Gary Liang <gary@arkade.com.au>
2019-03-20 00:09:17 +1100

    with state

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -1,12 +1,30 @@
 import React from 'react';
 
-const Home = ({ data }) => {
-
-  console.log('--');
-  console.log(data);
+const Home = ({ data, favoriteColorChange, favoriteFruitChange, genderChange, state }) => {
 
   return (
     <div className='home'>
+      <div className="filter">
+        <select className="favoriteColor" value={state.favoriteColor} onChange={favoriteColorChange}>
+          <option value="purple">purple</option>
+          <option value="turquoise">turquoise</option>
+          <option value="aquamarine">aquamarine</option>
+        </select>
+
+        <select className="favoriteFruit" value={state.favoriteFruit} onChange={favoriteFruitChange}>
+          <option value="mango">mango</option>
+          <option value="banana">banana</option>
+          <option value="lychee">lychee</option>
+        </select>
+
+        <select className="gender" value={state.gender} onChange={genderChange}>
+          <option value="male">male</option>
+          <option value="female">female</option>
+        </select>
+
+        <button>Filter</button>
+      </div>
+
       <div className="table-container" role="table" aria-label="Destinations">
         <div className="flex-table header" role="rowgroup">
           <div className="flex-row first" role="columnheader">age</div>
--- src/containers/Home/index.js
+++ src/containers/Home/index.js
@@ -5,13 +5,38 @@ import { default as HomeComponent } from '../../components/Home';
 import { loadHomeAPI } from '../../reducers/home';
 
 class Home extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      favoriteColor: "",
+      favoriteFruit: "",
+      gender: ""
+    }
+  }
+
   componentDidMount() {
     /* eslint-disable react/destructuring-assignment */
     this.props.loadHomeAPIProps();
   }
 
+  favoriteColorChange = (e) => {
+    this.setState({favoriteColor: e.target.value});
+  }
+
+  favoriteFruitChange = (e) => {
+    this.setState({favoriteFruit: e.target.value});
+  }
+
+  genderChange = (e) => {
+    this.setState({gender: e.target.value});
+  }
+
+
   render() {
     const { data } = this.props;
+
+    console.log(this.state);
 
     return (
       <div>
@@ -19,7 +44,13 @@ class Home extends Component {
           data.length === 0 ?
             <p>Loading....</p>
             :
-            <HomeComponent data={ data } />
+            <HomeComponent
+              data={data}
+              favoriteColorChange={this.favoriteColorChange}
+              favoriteFruitChange={this.favoriteFruitChange}
+              genderChange={this.genderChange}
+              state={this.state}
+            />
         }
       </div>
     );
