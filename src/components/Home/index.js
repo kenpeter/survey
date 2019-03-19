86c5cd440a7afb6df449b23e5589f149b5722f51
Gary Liang <gary@arkade.com.au>
2019-03-19 23:19:55 +1100

    backup

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -15,11 +15,28 @@ const Home = ({ data }) => {
           <div className="flex-row" role="columnheader">gender</div>
           <div className="flex-row" role="columnheader">name</div>
         </div>
+
+
+        
         <div className="flex-table row" role="rowgroup">
           <div className="flex-row first" role="cell">United Kingdom</div>
           <div className="flex-row" role="cell">Stonehenge, Windsor and Bath with Pub Lunch</div>
           <div className="flex-row" role="cell">19 Sep, 1p.m.</div>
           <div className="flex-row" role="cell">US$500</div>
+          <div className="flex-row" role="cell">US$500</div>
+        </div>
+        <div className="flex-table row" role="rowgroup">
+          <div className="flex-row first" role="cell">Canada</div>
+          <div className="flex-row" role="cell">Vancouver to Victoria and Butchart Gardens Tour</div>
+          <div className="flex-row" role="cell">23 Sep, 1:30p.m.</div>
+          <div className="flex-row" role="cell">US$387</div>
+          <div className="flex-row" role="cell">US$500</div>
+        </div>
+        <div className="flex-table row" role="rowgroup">
+          <div className="flex-row first" role="cell">Canada</div>
+          <div className="flex-row" role="cell">Vancouver to Victoria and Butchart Gardens Tour</div>
+          <div className="flex-row" role="cell">23 Sep, 1:30p.m.</div>
+          <div className="flex-row" role="cell">US$387</div>
           <div className="flex-row" role="cell">US$500</div>
         </div>
         <div className="flex-table row" role="rowgroup">
